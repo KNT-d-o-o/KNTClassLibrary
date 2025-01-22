d2b1a518a5619223bbb8655684d18824312e55fd
fstanonik <franc.stanonik@knt.si>
2025-01-22 11:49:18 +0100

    Page size, OverflowY
    Page size: one setting
    OverflowY: for smaller windows (users, user groups)

--- KNTCommon.Blazor/Services/SharedContainerCommon.cs
+++ KNTCommon.Blazor/Services/SharedContainerCommon.cs
@@ -9,5 +9,6 @@ namespace KNTCommon.Blazor.Services
         public bool Additional = false;
         static public string actionTitle = string.Empty;
         public int LoggedPower = 99;
+        public int pageSize = 5;
     }
 }
