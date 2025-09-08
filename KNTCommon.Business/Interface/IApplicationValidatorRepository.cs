6691c2a36af90958cf3dd7903a9da2dba295db90
MihaGir <miha.girandon@knt.si>
2025-09-08 11:37:05 +0200

    more error detection

--- KNTCommon.Business/Interface/IApplicationValidatorRepository.cs
+++ KNTCommon.Business/Interface/IApplicationValidatorRepository.cs
@@ -1,9 +1,14 @@
-﻿namespace KNTCommon.Business.Interface
+﻿using Microsoft.EntityFrameworkCore;
+using KNTCommon.Business.Repositories;
+
+namespace KNTCommon.Business.Interface
 {
     public interface IApplicationValidatorRepository
     {        
         public List<string> EfTableValidation<TContext>(params string[] excludeTables) where TContext : class, IDisposable, new();
         List<string> FindCorruptData();
+        List<string> DbEfTableValidation<TContext>(params string[] excludeTables) where TContext : DbContext, IDisposable, new();        
+        IEnumerable<ValidatorMissingTranslation> GetMissingEntityTranslation(params string[] excludeTables);
 
     }
 }
